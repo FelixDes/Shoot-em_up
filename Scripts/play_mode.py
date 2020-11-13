a7e024f48b8f758624865e4b18f3f88eb941f35d
F <vip.zatebe@mail.ru>
2020-11-13 20:43:36 +0300

    Moving_added

--- Scripts/play_mode.py
+++ Scripts/play_mode.py
@@ -8,13 +8,20 @@ class Play_mode():
         self.str_dict = {}
         self.fill_str()
         self.field = []
+        self.fill_field()
+
+        self.player_x_from_list = 0
+        self.field_h = int(self.str_dict.get("field_h"))
+        self.field_w = int(self.str_dict.get("field_w"))
+        self.frame_h = int(self.str_dict.get("h"))
+        self.frame_w = int(self.str_dict.get("w"))
 
         self.FPS = int(self.str_dict.get("FPS"))
 
     def fill_field(self):
         self.field = [[0 for j in range(int(self.str_dict.get("field_w")))]
                       for i in range(int(self.str_dict.get("field_h")))]
-        self.field[-1][self.field[-1] // 2] = "p"
+        self.field[-1][len(self.field[-1]) // 2] = "p"
 
     def fill_str(self):
         self.str_dict = {}
@@ -33,9 +40,22 @@ class Play_mode():
                 if i.type == pygame.QUIT:
                     exit()
                 elif i.type == pygame.KEYDOWN and i.key == pygame.K_RIGHT:
-                    pass
+                    self.p_do(1)
                 elif i.type == pygame.KEYDOWN and i.key == pygame.K_LEFT:
-                    pass
+                    self.p_do(-1)
                 elif i.type == pygame.KEYDOWN and (i.key == pygame.K_SPACE or i.key == pygame.K_UP):  # стрельба
-                    pass
+                    self.p_do(0)
             pygame.display.update()
+
+    def p_do(self, dir):
+        index_p = self.field[-1].index("p")
+        if dir == 1 and 0 <= self.field[-1].index("p") <= int(self.str_dict.get('field_w')) - 2:
+            self.field[-1][index_p], self.field[-1][index_p + 1] = self.field[-1][index_p + 1], self.field[-1][
+                index_p]
+            print(self.field)
+            self.player_x_from_list += 1 / self.field_w * self.frame_w
+        elif dir == -1 and 1 <= self.field[-1].index("p") <= int(self.str_dict.get('field_w')) - 1:
+            self.field[-1][index_p], self.field[-1][index_p - 1] = self.field[-1][index_p - 1], self.field[-1][
+                index_p]
+            print(self.field)
+            self.player_x_from_list -= 1 / self.field_w * self.frame_w
