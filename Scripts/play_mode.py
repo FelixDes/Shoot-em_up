45ae9e3806db6053729cc744f5405b9f4d34ee60
F <vip.zatebe@mail.ru>
2020-12-06 10:45:10 +0300

    Enemies_added

--- Scripts/play_mode.py
+++ Scripts/play_mode.py
@@ -1,6 +1,7 @@
 import csv
 
 import pygame
+import random
 
 pygame.font.init()
 
@@ -33,16 +34,33 @@ class Play_mode():
         self.frame_w = int(str_dict.get("w"))
         self.FPS = int(str_dict.get("FPS"))
         self.sc = pygame.display.set_mode((self.frame_w, self.frame_h), pygame.RESIZABLE)
-        self.lvl = 1
+        self.lvl = 0
         self.hp = 4
         self.player_shift = 5
-        self.player = Player_Ship(self.frame_w//2, self.frame_h-int(str_dict.get('ship_y')))
-
+        self.player = Player_Ship(self.frame_w // 2, self.frame_h - int(str_dict.get('ship_y')))
+        self.enemies = []
+        self.wave_len = 3
+        self.enemy_shift = 5
+    def end_game(self):
+        exit(0)
     def run(self):
         while True:
             clock = pygame.time.Clock()
             clock.tick(self.FPS)
-            self.redraw_window()
+
+            if self.hp <= 0 or self.player.hp <= 0:
+                self.end_game()
+
+
+            if len(self.enemies) == 0:
+                self.lvl += 1
+                self.wave_len += 4
+                self.enemy_shift += 1
+                for i in range(self.wave_len):
+                    enemy = Enemy_Ship(random.randrange(50, self.frame_w - 150), random.randrange(-1500, -100))
+                    self.enemies.append(enemy)
+            for enemy in self.enemies:
+                enemy.draw(self.sc)
             # цикл обработки событий
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -56,6 +74,13 @@ class Play_mode():
                 self.player.y -= self.player_shift
             elif keys[pygame.K_DOWN] and self.player.y + self.player_shift + int(str_dict.get("ship_y")) < self.frame_h:
                 self.player.y += self.player_shift
+            for enemy in self.enemies[:]:
+                enemy.mover(self.enemy_shift)
+                if enemy.y + int(str_dict.get("ship_y")) > self.frame_h:
+                    self.hp -= 1
+                    self.enemies.remove(enemy)
+            self.redraw_window()
+
 
     def redraw_window(self):
         pygame.display.update()
@@ -69,11 +94,12 @@ class Play_mode():
 
 
 class Super_Ship:
-    def __init__(self, x, y, hp=100):
+    def __init__(self, x, y, hp=10):
         self.x = x
         self.y = y
         self.hp = hp
         self.ship_asset = ENEMY_SHIP_PNG
+        self.bullet_asset = BULLET_PNG
         self.bullets_cool_down = 0
         self.bullets = []
 
@@ -82,7 +108,20 @@ class Super_Ship:
 
 
 class Player_Ship(Super_Ship):
-    def __init__(self, x, y, hp=1):
+    def __init__(self, x, y, hp=10):
         super().__init__(x, y, hp)
         self.ship_asset = PLAYER_SHIP_PNG
         self.bullet_asset = BULLET_PNG
+        self.mask = pygame.mask.from_surface(self.ship_asset)
+        self.max_hp = hp
+
+
+class Enemy_Ship(Super_Ship):
+    def __init__(self, x, y, hp=10):
+        super().__init__(x, y, hp)
+        self.ship_asset = ENEMY_SHIP_PNG
+        self.bullet_asset = BULLET_PNG
+        self.mask = pygame.mask.from_surface(self.ship_asset)
+
+    def mover(self, shift):
+        self.y += shift
