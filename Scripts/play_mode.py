08f84372154f97687ad92d8b7c6a638821daf9ef
F <vip.zatebe@mail.ru>
2020-12-10 22:36:05 +0300

    collision_shoot_ability_added

--- Scripts/play_mode.py
+++ Scripts/play_mode.py
@@ -1,33 +1,47 @@
 import csv
 import random
+import sys
+import threading
+import time
 from math import sqrt
 
 import pygame
 
 pygame.font.init()
 
-str_dict = {}
-
 
 def fill_str():
+    str_dict = {}
     with open('Res/CSV/const.csv', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
             str_dict[row[0]] = row[1]
+        return str_dict
 
 
-fill_str()
+str_dict = fill_str()
 
 BACKGROUND = pygame.image.load("Res/Assets/space.png")
 PLAYER_SHIP_PNG = pygame.transform.scale(pygame.image.load("Res/Assets/p_ship.png"),
                                          (int(str_dict.get('ship_y')), int(str_dict.get('ship_y'))))
 ENEMY_SHIP_PNG = pygame.transform.scale(pygame.image.load("Res/Assets/enemy.png"),
                                         (int(str_dict.get('ship_x')), int(str_dict.get('ship_y'))))
-# BOSS_SHIP_PNG = pygame.transform.scale(pygame.image.load("Res/Assets/boss_ship.png"), (int(str_dict.get('ship_y')), int(str_dict.get('ship_y'))))
+BOSS_SHIP_PNG = pygame.transform.scale(pygame.image.load("Res/Assets/boss_ship.png"),
+                                       (int(str_dict.get('ship_y')), int(str_dict.get('ship_y'))))
 BULLET_PNG = pygame.transform.scale(pygame.image.load("Res/Assets/bul.png"),
                                     (int(str_dict.get('bullet_x')), int(str_dict.get('bullet_y'))))
+
 BASIC_FONT = pygame.font.SysFont("comicsans", 20)
 GAME_OVER_FONT = pygame.font.SysFont("comicsans", 60)
+
+exit_flag = False
+
+
+def exit_for_time(t):
+    global exit_flag
+    time.sleep(t)
+    exit_flag = True
+    sys.exit()
 
 
 class Play_mode():
@@ -37,15 +51,22 @@ class Play_mode():
         self.FPS = int(str_dict.get("FPS"))
         self.sc = pygame.display.set_mode((self.frame_w, self.frame_h), pygame.RESIZABLE)
         self.lvl = 0
-        self.hp = 4
-        self.player_shift = 5
+        self.lives = 4
+        self.player_shift = 7
         self.player = Player_Ship(self.frame_w // 2, self.frame_h - int(str_dict.get('ship_y')))
         self.enemies = []
-        self.wave_len = 3
+        self.wave_len = 0
         self.enemy_shift = 5
+        self.bull_shift = 7
         self.clock = pygame.time.Clock()
 
     def end_game(self):
+        global exit_flag
+        time = 4
+        # _thread.start_new_thread(exit_for_time, (time,))
+        exit_tread = threading.Thread(target=exit_for_time, args=(time,))
+        exit_tread.start()
+        # exit_tread.join()
         while True:
             self.sc.fill((0, 0, 0))
             game_over_txt = GAME_OVER_FONT.render(f"GAME OVER", 1, (255, 255, 255))
@@ -53,15 +74,17 @@ class Play_mode():
                                          (self.frame_h - game_over_txt.get_height()) // 2))
             self.clock.tick(self.FPS)
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT or exit_flag:
+                    exit_flag = False
                     exit(0)
+
             pygame.display.update()
 
     def run(self):
         while True:
             self.clock.tick(self.FPS)
 
-            if self.hp <= 0 or self.player.hp <= 0:
+            if self.lives <= 0 or self.player.hp <= 0:
                 self.end_game()
 
             if len(self.enemies) == 0:
@@ -89,11 +112,24 @@ class Play_mode():
                 self.player.y -= self.player_shift * coef
             if keys[pygame.K_DOWN] and self.player.y + self.player_shift + int(str_dict.get("ship_y")) < self.frame_h:
                 self.player.y += self.player_shift * coef
+            if keys[pygame.K_SPACE]:
+                self.player.shoot()
             for enemy in self.enemies[:]:
                 enemy.mover(self.enemy_shift)
-                if enemy.y + int(str_dict.get("ship_y")) > self.frame_h:
-                    self.hp -= 1
+                enemy.move_bullets(self.bull_shift, self.player)
+
+                if random.randrange(0, 120) == 1:
+                    enemy.shoot()
+
+                if collide(enemy, self.player):
+                    self.player.hp -= 10
                     self.enemies.remove(enemy)
+
+                if enemy.y + int(str_dict.get("ship_y")) > self.frame_h:
+                    self.lives -= 1
+                    self.enemies.remove(enemy)
+
+            self.player.move_bullets(-self.bull_shift, self.enemies)
             self.redraw_window()
 
     def redraw_window(self):
@@ -101,13 +137,41 @@ class Play_mode():
         self.sc.blit(BACKGROUND, (0, 0))
         # вывод текстовой информации
         lvl_lable = BASIC_FONT.render(f"Уровень: {self.lvl}", 1, (255, 255, 255))
-        hp_lable = BASIC_FONT.render(f"Жизни: {self.hp}", 1, (255, 255, 255))
+        lives_lable = BASIC_FONT.render(f"Жизни: {self.lives}", 1, (255, 255, 255))
         self.sc.blit(lvl_lable, (self.frame_w - lvl_lable.get_width() - 10, 5))
-        self.sc.blit(hp_lable, (self.frame_w - hp_lable.get_width() - 10, 10 + lvl_lable.get_height()))
+        self.sc.blit(lives_lable, (self.frame_w - lives_lable.get_width() - 10, 10 + lvl_lable.get_height()))
         self.player.draw(self.sc)
 
 
+class Super_Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = BULLET_PNG
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, sc):
+        sc.blit(self.img, (self.x, self.y))
+
+    def mover(self, shift):
+        self.y += shift
+
+    def off_screen(self, h):
+        return not (h > self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
+
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (int(offset_x), int(offset_y))) != None
+
+
 class Super_Ship:
+    COOLDOWN = 30
+
     def __init__(self, x, y, hp=10):
         self.x = x
         self.y = y
@@ -119,16 +183,52 @@ class Super_Ship:
 
     def draw(self, sc):
         sc.blit(self.ship_asset, (self.x, self.y))
+        for bullet in self.bullets:
+            bullet.draw(sc)
+
+    def move_bullets(self, shift, obj):
+        self.cool_down()
+        for bullet in self.bullets:
+            bullet.mover(shift)
+            if bullet.off_screen(int(str_dict.get('h'))):
+                self.bullets.remove(bullet)
+            elif bullet.collision(obj):
+                obj.hp -= 10
+                self.bullets.remove(bullet)
+
+    def cool_down(self):
+        if self.bullets_cool_down >= self.COOLDOWN:
+            self.bullets_cool_down = 0
+        elif self.bullets_cool_down > 0:
+            self.bullets_cool_down += 1
+
+    def shoot(self):
+        if self.bullets_cool_down == 0:
+            bullet = Super_Bullet(self.x, self.y)
+            self.bullets.append(bullet)
+            self.bullets_cool_down = 1
 
 
 class Player_Ship(Super_Ship):
-    def __init__(self, x, y, hp=10):
+    def __init__(self, x, y, hp=100):
         super().__init__(x, y, hp)
         self.ship_asset = PLAYER_SHIP_PNG
         self.bullet_asset = BULLET_PNG
         self.mask = pygame.mask.from_surface(self.ship_asset)
         self.max_hp = hp
 
+    def move_bullets(self, shift, objs):
+        self.cool_down()
+        for bullet in self.bullets:
+            bullet.mover(shift)
+            if bullet.off_screen(int(str_dict.get('h'))):
+                self.bullets.remove(bullet)
+            else:
+                for obj in objs:
+                    if bullet.collision(obj):
+                        objs.remove(obj)
+                        if bullet in self.bullets:
+                            self.bullets.remove(bullet)
 
 
 class Enemy_Ship(Super_Ship):
