c2762c284f1e88ebbe7bfe6586c424c5fe8158dc
Felix <felixdesyatirikov@gmail.com>
2024-03-26 17:17:29 +0300

    Remove venv

--- venv/Lib/site-packages/pygame_widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from pygame_widgets.textbox import TextBox
-from pygame_widgets.button import Button, ButtonArray
-from pygame_widgets.slider import Slider
-from pygame_widgets.animations.animation import AnimationBase, Translate, Resize
--- venv/Lib/site-packages/pygame_widgets/exceptions/exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidParameter(Exception):
-    """ Exception called if an invalid parameter is passed into an animation"""
-
-
-class InvalidParameterType(Exception):
-    """ Exception called if an invalid parameter type is passed into an animation"""
